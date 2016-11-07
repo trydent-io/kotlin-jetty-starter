d7bded1882d1f0ba98dcf3d5cbb5f3245f313662
Edgar Espina <espina.edgar@gmail.com>
2016-11-07 08:41:06 -0300

    more kotlin like

--- src/main/kotlin/app/App.kt
+++ src/main/kotlin/app/App.kt
@@ -12,9 +12,17 @@ class App : Jooby() {
    */
   init {
 
-    get("/", { -> "Hi Kotlin" })
+    get ("/") { ->
+      "Hi Kotlin"
+    }
 
-    get("/:name", { req -> "Hi " + req.param("name").value() })
+    get ("/:name") { req ->
+      "Hi " + req.param("name").value()
+    }
+
+    get ("/rsp/:name") { req, rsp ->
+      rsp.send("Hi " + req.param("name").value())
+    }
   }
 
 }
